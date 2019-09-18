21fb9ee62846a8afe98cb4ca87cd9a195d7ac744
Alexander Nygaard <2021.alexander.nygaard@uwcisak.jp>
2019-09-18 23:54:29 +0900

    Improved the calculator

--- Homework/19. sept/calculator.sh
+++ Homework/19. sept/calculator.sh
@@ -3,54 +3,51 @@
 # This program works like a calculator, 
 # and does a certain arithmetic operation (chosen by the user) on the arguments given
 
-##### DOES NOT WORK
-#### CHECK PYTHON PROGRAM
+# Save the arguments list as a variable
+args=("$*")
 
+# Check if first argument is "add"
+# NOTE: In bash, the index 0 is not the first argument, but rather the program name
+if [ $1 == "add" ]; then
+    echo "Doing plus"
+    # Defines output variable
+    output=0
+    # Loops through the arguments, and adds them to the output
+    for i in $args
+    do
+        ((output+=$i))
+    done
 
-firstIteration=true
-arithmetic=""
+# Check if first argument is "minus"
+elif [ $1 == "minus" ]; then
+    echo "Doing minus"
+    # Definines output variable as the first argument times 2
+    # The reason for this is that the loop subtracts the first arguement from this initial variable.
+    # If you do not multiply by 2, the variable would be equal 0 anyways after the first iteration of the loop
+    (( output=$2*2 ))
+    # Loops through the arguements, subtracts them from the first argument
+    for i in $args
+    do
+        ((output-=$i))
+    done
 
-for i in $*
-do
-    if (( $firstIteration )); then
-        if (( $i == "add" || $i == "minus" || $i == "product" )); then
-            firstIteration=$false
-            
-            
-            if (( $i == "add" )); then
-                arithmetic="add"
-                output=0
+#### MULTIPLICATION DOESNT WORK
 
-            elif (( $i == "minus" )); then
-                arithmetic="minus"
-                output=0
+# Check if first argument is "product"
+elif [ $1 == "product" ]; then
+    echo "Doing multiplication"
+    output=1
+    # Loops through the arguments and multiplies them together
+    for i in $args
+    do
+        echo "In for loop"
+        echo "I = $i"
+        echo "Output = $output"
+        output=$(($output * $i))
+    done
 
+fi
 
-            elif (( $i == "product" )); then
-                arithmetic="product"
-                output=1
-            
-            fi
-
-
-        else
-            echo "Error. Input wrong. Choose an arithmetic operaton"
-            break
-        fi
-
-
-    else
-        # MATH DONE HERE
-
-        if (( $arithmetic == "add" )); then
-            #(( output=$output+$i))
-            echo "success"
-    
-
-        fi
-
-
-    fi
-
-done
+# Prints output
+echo "The output is $output"
 
